fc0e0898681e10774540633638f71103002e82f4
Adam Watchmaker <watchmaker1337@gmail.com>
2024-04-19 16:36:51 +0200

    add Props

--- src/components/Searching.tsx
+++ src/components/Searching.tsx
@@ -1,10 +1,19 @@
+import { ChangeEvent} from "react";
+type Props ={
+    value:string;
+    setValue: (e:ChangeEvent<HTMLInputElement>) => void;
+    handleBtn: () =>void;
+}
 import "../styles/Searching.css"
-const Searching = () =>{
+const Searching = (props:Props) =>{
+
+    const {value,setValue,handleBtn} = props;
+    console.log(setValue);
 
     return(
         <div className="searching container">
-            <input type="text" className="search_input" placeholder="Name of your product" />
-            <button className="btn search_btn">Search</button>
+            <input type="text" className="search_input" placeholder="Name of your product" value={value} onChange={setValue} />
+            <button className="btn search_btn" onClick={handleBtn}>Search</button>
         </div>
     )
 }
